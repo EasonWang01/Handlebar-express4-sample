91ffc66e008dd566c770d0028d701063bd76419d
Easonwang01 <jason40115@hotmail.com>
2016-01-14 20:38:01 +0800

    add

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/as', function (req, res) {
+    res.render('home');
+});
+app.get('/', function (req, res) {
+    res.render('main');
+});
+
+
+app.listen(3000);
